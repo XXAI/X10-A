f2a4c01efe4d9ad02a0d94449e6ee84c72524910
ragucaru <ragucaru80@gmail.com>
2021-10-04 20:21:00 -0500

    modificando rutas2

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,10 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('login', [ 'as' => 'login', 'uses' => 'Auth\LoginController@showLogin']);
     Route::get('/', function () { return Redirect::to('login'); });
     Route::post('sign-in','Auth\LoginController@doLogin');
-    Route::middleware('auth')->get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+    //Route::middleware('auth')->get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
 
     Route::view('/asistencia/{rfc}','infoRh');
-    /* Route::view('/kardex','reportes/kardex');
-    Route::view('/kardex/{id}','reportes/kardexUsuario'); */
-
-    //Route::view('/prueba','prueba2');
-
-
-    //Route::get('/repgral','reporteGralController@index');
-
-    //Route::view('/reporte-mensual','reportes/mensual');
-    //Route::view('/reporte-pdf-mensual','reportes/reporte-mensual');
-
-
+   /*
     Route::middleware('auth')->get('/dashboard', 'DashboardController@index');
     Route::middleware('auth')->get('/reporte-mensual', 'DashboardController@mensual');
     Route::middleware('auth')->get('/reporte-direccion', 'DashboardController@direccion');
@@ -29,5 +18,20 @@ Route::group(['middleware' => 'web'], function () {
 
     Route::middleware('auth')->get('/cardex', 'DashboardController@cardex');
     Route::middleware('auth')->get('/checadas', 'DashboardController@checadas');
+    Route::middleware('rutabase')->get('//consulta-asistencia', 'API\reporteController@consulta_checadas');*/
+    Route::group(['middleware' => 'auth'], function() {
+        Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+        Route::get('/dashboard', 'DashboardController@index');
+        Route::get('/reporte-mensual', 'DashboardController@mensual');
+        Route::get('/reporte-direccion', 'DashboardController@direccion');
+        Route::get('/reporte-capturistas', 'DashboardController@capturistas');
+        Route::get('/reporte-trimestral', 'DashboardController@trimestral');
+        Route::get('/empleado', 'DashboardController@empleado');
+        Route::get('/register', 'DashboardController@register');
+    
+        Route::get('/cardex', 'DashboardController@cardex');
+        Route::get('/checadas', 'DashboardController@checadas');
+        
+    });
     Route::middleware('rutabase')->get('//consulta-asistencia', 'API\reporteController@consulta_checadas');
 });
