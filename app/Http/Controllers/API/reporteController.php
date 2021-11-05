193d752a344b1d19948738e804e3dbc710642314
ragucaru <ragucaru80@gmail.com>
2021-11-05 05:44:48 -0600

    SE corrige variable de checado fuera de horario

--- app/Http/Controllers/API/reporteController.php
+++ app/Http/Controllers/API/reporteController.php
@@ -419,13 +419,15 @@ class reporteController extends Controller
     
     
                             $inicio_sal_fuera=new Carbon($fecha_eval." ".$var_reglas[$fecha_evaluar->dayOfWeekIso]->InicioChecarSalida.":00.000"); 
+                            
                             $final_sal_fuera=$fecha_eval."T".'23:59:59.000';  
-                            $inicio_sal_fuera->subHours(2);
+                            //$inicio_sal_fuera->subHours(2);
+                          
                             $final_entra_fuera=$inicio_sal_fuera->subHours(2);
                             $final_entra_fuera->subMinute();
                             $final_entra_fuera= str_replace(" ", "T", $final_entra_fuera);
                             $inicio_sal_fuera= str_replace(" ", "T", $inicio_sal_fuera);
-                        //    dd($inicio_sal_fuera);
+                          // dd($inicio_sal_fuera);
                             $trab=$diatrab;
                             if ($diatrab!=0 || $festivo==1 )
                                 {
@@ -463,7 +465,7 @@ class reporteController extends Controller
                                // return "InicioSalida: ". $inicio_sal."  SAlidadddddddda: ".$final_sal;         
                                 
                            
-                            
+                           //  dd($inicio_sal_fuera);
                            
                                                    
                             
