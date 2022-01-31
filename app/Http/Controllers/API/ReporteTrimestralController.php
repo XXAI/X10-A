ebcfdfd78dab7dd1858c03d8c26b8f6c413778ed
ramon cabrera <ragucaru80@gmail.com>
2022-01-31 13:39:35 -0600

    trimestral difiere

--- app/Http/Controllers/API/ReporteTrimestralController.php
+++ app/Http/Controllers/API/ReporteTrimestralController.php
@@ -81,7 +81,7 @@ class ReporteTrimestralController extends Controller
             }
         }
 
-        $catalogo_trimestre = [ 1 =>[1,2,3], 2 => [4,5,6], 3=> [7,8,9], 4=> [11]];
+        $catalogo_trimestre = [ 1 =>[1,2,3], 2 => [4,5,6], 3=> [7,8,9], 4=> [10,11,12]];
         
         $empleados_trimestral = [];
 
